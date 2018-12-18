ae9e08ad1eea0708c04979392797c1856b7b277a
Jinux <jinguol999@163.com>
2018-12-18 21:35:37 +0800

    return in finally is bad

---
+++ src/com/jinux/agilejava/playground/ExceptionTest.java
@@ -0,0 +1,27 @@
+package com.jinux.agilejava.playground;
+
+import org.junit.jupiter.api.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ExceptionTest {
+    @Test
+    void testReturnInFinish() throws MalformedURLException {
+        try {
+            new URL("hello");
+        } catch (MalformedURLException e) {
+            log("haha");
+            throw e;
+        } finally {
+            log("hello");
+            return;
+        }
+    }
+
+    private void log(String haha) {
+        Logger.getGlobal().log(Level.ALL, haha);
+    }
+}
